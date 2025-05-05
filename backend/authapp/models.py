b5bb9482eb97860ea3537e2914f8d4fee1118a13
Syed Asad Jamil <syedasad1410@gmail.com>
2025-05-05 17:18:17 +0500

    dependency updated

---
+++ backend/authapp/models.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
+    gender = models.CharField(max_length=10, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
---
+++ backend/backend/urls.py
@@ -0,0 +1,14 @@
+"Django imports"
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.http import HttpResponse
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('authapp.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
