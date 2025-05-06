112df2f632d5fdb832a05124b4b19f7064e66f0e
Syed Asad Jamil <syedasad1410@gmail.com>
2025-05-06 22:39:14 +0500

    Features added

--- backend/authapp/models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-class CustomUser(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
-    gender = models.CharField(max_length=10, null=True, blank=True)
-    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
