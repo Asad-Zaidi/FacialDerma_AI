776b0b05dff4e857bd308ead037ae932f47813e8
Syed Asad Jamil <syedasad1410@gmail.com>
2025-05-10 22:39:27 +0500

    refactor the code

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-// import './App.css';
-// import React from 'react';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Home from './Pages/Home';
-// import Analysis from './Pages/Analysis';
-// import About from './Pages/About';
-// import Profile from './Pages/Profile';
-// import LoginForm from './Pages/Login';
-// import SignupForm from './Pages/Signup';
-// import ForgetPassword from './Pages/ForgetPassword';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route exact path="/" element={<Home />} />
-//         <Route exact path="/home" element={<Home />} />
-//         <Route exact path="/Analysis" element={<Analysis />} />
-//         <Route exact path="/about" element={<About />} />
-//         <Route exact path="/Profile" element={<Profile />} />
-//         <Route path="/login" element={<LoginForm />} />
-//         <Route path="/signup" element={<SignupForm />} />
-//         <Route path="/forget-password" element={<ForgetPassword />} />
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import AppRouter from './Routes/AppRouter';
 import './App.css';
--- src/Routes/AppRouter.jsx
+++ src/Routes/AppRouter.jsx
@@ -1,40 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Home from "../Pages/Home";
-// import About from "../Pages/About";
-// import Profile from "../Pages/Profile";
-// import Analysis from "../Pages/Analysis";
-// import Login from "../Pages/Login";
-// import Signup from "../Pages/Signup";
-// import ForgetPassword from "../Pages/ForgetPassword";
-// import PrivateRoute from "./PrivateRoutes";
-
-// const AppRouter = () => {
-//     return (
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path="/" element={<Home />} />
-//                 <Route path="/home" element={<Home />} />
-//                 <Route path="/about" element={<About />} />
-//                 <Route path="/login" element={<Login />} />
-//                 <Route path="/signup" element={<Signup />} />
-//                 <Route path="/forget-password" element={<ForgetPassword />} />
-//                 <Route path="/profile" element={
-//                     <PrivateRoute>
-//                         <Profile />
-//                     </PrivateRoute>
-//                 } />
-//                 <Route path="/analysis" element={
-//                     <PrivateRoute>
-//                         <Analysis />
-//                     </PrivateRoute>
-//                 } />
-//             </Routes>
-//         </BrowserRouter>
-//     );
-// }
-
-// export default AppRouter;
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import About from "../Pages/About";
@@ -55,7 +18,7 @@ const AppRouter = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/forget-password" element={<ForgetPassword />} />
-                
+
                 <Route path="/profile" element={
                     <PrivateRoute>
                         <Profile />
