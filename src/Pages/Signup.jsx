d4fcf719fa548cc84ca6bf76f58bcb3e220b0d80
haseebt456 <m.haseebtufail@gmail.com>
2025-05-11 19:51:30 +0500

    update fetch link

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -35,7 +35,7 @@ const SignupForm = () => {
         }
 
         try {
-            const response = await fetch('http://127.0.0.1:8000/api/auth/signup/', {
+            const response = await fetch('http://localhost:5000/api/auth/register/', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(formData),
