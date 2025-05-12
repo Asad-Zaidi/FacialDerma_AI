3df5e4743c07f5af07c822d09011e68cb3ac5999
haseebt456 <m.haseebtufail@gmail.com>
2025-05-12 16:29:27 +0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
-import AppRouter from './Routes/AppRouter';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Analysis from "./Pages/Analysis";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
+import ForgetPassword from "./Pages/ForgetPassword";
+import PrivateRoute from "./Routes/PrivateRoutes";
 
-function App() {
-  return <AppRouter />;
-}
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/forget-password" element={<ForgetPassword />} />
 
-export default App;
+                <Route path="/Profile" element={
+                    <PrivateRoute>
+                        <Profile />
+                    </PrivateRoute>
+                } />
+                <Route path="/analysis" element={
+                    <PrivateRoute>
+                        <Analysis />
+                    </PrivateRoute>
+                } />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRouter;
