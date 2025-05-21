d59ce906ef058ff7e1a1b8dc6012067211d5fb12
Syed Asad Jamil <syedasad1410@gmail.com>
2025-05-21 14:31:05 +0500

    fixed navigate to login after refreshing

--- src/Routes/PrivateRoutes.jsx
+++ src/Routes/PrivateRoutes.jsx
@@ -1,11 +1,29 @@
+// import { Navigate } from 'react-router-dom';
+// import { useContext } from 'react';
+// import { AuthContext } from '../contexts/AuthContext';
+
+// const PrivateRoute = ({ children, roles }) => {
+//   const { user, accessToken } = useContext(AuthContext);
+
+//   if (!accessToken || !user) return <Navigate to="/login" replace />;
+//   if (roles && !roles.includes(user.role)) return <Navigate to="/" replace />;
+
+//   return children;
+// };
+
+// export default PrivateRoute;
+
 import { Navigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 const PrivateRoute = ({ children, roles }) => {
-  const { user, accessToken } = useContext(AuthContext);
+  const { user, accessToken, loading } = useContext(AuthContext);
+
+  if (loading) return <div>Loading...</div>;
 
   if (!accessToken || !user) return <Navigate to="/login" replace />;
+
   if (roles && !roles.includes(user.role)) return <Navigate to="/" replace />;
 
   return children;
